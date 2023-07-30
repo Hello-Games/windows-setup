fa93ba4c34f5a2bf1f258d26cd5dc91ec0146f9b
HgIain <62030951+HgIain@users.noreply.github.com>
2023-07-30 19:54:05 +0100

    Ignore remote desktop errors

--- dev-setup.ps1
+++ dev-setup.ps1
@@ -59,7 +59,7 @@ Set-NetConnectionProfile -NetworkCategory Private
 # Disable Fast boot
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power" -Name "HiberbootEnabled" -Type DWord -Value 0
 
-Enable-RemoteDesktop
+Enable-RemoteDesktop -ErrorAction SilentlyContinue
 
 #install this outside boxstarter since it doesn't handle the vs packages
 choco install visualstudio2022professional -y --package-parameters "--add Microsoft.VisualStudio.Workload.NativeDesktop Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Core Microsoft.VisualStudio.Component.Windows10SDK.19041 Microsoft.VisualStudio.Component.VC.v141.x86.x64 Microsoft.VisualStudio.Component.VC.Tools.x86.x64 Microsoft.VisualStudio.Component.VC.Llvm.Clang Microsoft.VisualStudio.Component.VC.Llvm.ClangToolset Microsoft.VisualStudio.Component.VC.ASAN"
